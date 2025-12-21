10f727cc058fec3cc8dd1c37fe9139908fdb297f
Dima0913 <153444026+Dima0913@users.noreply.github.com>
2025-12-21 11:06:40 +0200

    Delete my-react-app directory

--- my-react-app/src/config/env.ts
+++
@@ -1,8 +0,0 @@
-class EnvConfig {
-    static readonly API_URL: string = import.meta.env.VITE_API_URL as string;
-
-    // Якщо потім треба буде додати інші змінні:
-    // static readonly APP_NAME: string = import.meta.env.VITE_APP_NAME as string;
-}
-
-export default EnvConfig;
